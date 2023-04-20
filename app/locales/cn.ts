55babb449d36b2747978a98735f7907497a3a1b5
Ben Ng <101241940+bentwnghk@users.noreply.github.com>
2023-04-20 16:39:10 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -90,7 +90,7 @@ const cn = {
       CheckUpdate: "检查更新",
       IsChecking: "正在检查更新...",
       FoundUpdate: (x: string) => `发现新版本：${x}`,
-      GoToUpdate: "前往更新",
+      GoToUpdate: "可以更新",
     },
     SendKey: "发送键",
     Theme: "主题",
