85fe9a4add4d9f0db29822a54e0955c4393c6270
Ben <mr.ng@mystudies.net>
2023-05-08 01:16:58 +0800

    Fix auto-select voices

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -83,7 +83,7 @@ function createEmptySession(): ChatSession {
     lastSummarizeIndex: 0,
     mask: createEmptyMask(),
     ttsConfig: {
-      voice: (typeof window !== "undefined" && window.speechSynthesis.getVoices().find(voice => voice.name === "Microsoft Aria")) ? "Microsoft Aria Online (Natural) - English (United States)" : "Samantha",
+      voice: window.speechSynthesis.getVoices().find(voice => voice.name === "Microsoft Aria") ? "Microsoft Aria Online (Natural) - English (United States)" : "Samantha",
       lang: "en-US",
     },
   };
