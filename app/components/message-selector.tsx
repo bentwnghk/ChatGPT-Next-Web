9ad9f6cbdd92015a4cd91ee87f23d510c0f28241
Ben <mr.ng@mystudies.net>
2023-05-26 09:53:30 +0800

    Fix date

--- app/components/message-selector.tsx
+++ app/components/message-selector.tsx
@@ -200,7 +200,7 @@ export function MessageSelector(props: {
               </div>
               <div className={styles["body"]}>
                 <div className={styles["date"]}>
-                  {new Date(m.date).toLocaleString()}
+                  {m.date}
                 </div>
                 <div className={`${styles["content"]} one-line`}>
                   {m.content}
