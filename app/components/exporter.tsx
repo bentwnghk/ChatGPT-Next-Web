12cac3581dc6db0692da9bfa945e0f9676df54c0
Ben <mr.ng@mystudies.net>
2023-06-16 21:12:18 +0800

    Update name

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,9 +437,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>Mr.5️⃣ ChatGPT+</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              ai7.mister5.net
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
